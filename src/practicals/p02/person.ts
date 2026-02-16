e5be3f02c72265aa0d71a4af21f5954a5f80a7e1
srnTone <68011813@kmitl.ac.th>
2026-02-16 09:52:55 +0700

    p02.1

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,10 +1,22 @@
 export class Person {
     firstname?: string;
     lastname?: string;
-    private age?: number;
+    private _age?: number = 10;
+    static COUNTRY: string = 'Thailand';
 
     constructor(firstname?: string, lastname?: string) {
         this.firstname = firstname;
         this.lastname = lastname;
     }
+
+    setAge(n: number){
+        this._age = n;
+    }
+    getAge(){
+        return this._age;
+    }
+    getFullName(){
+        return `${this.firstname} ${this.lastname}`
+    }
+
 }
