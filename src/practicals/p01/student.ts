6014be254e3c77ff5a2ce9ac23cbc7c5d0c32505
srnTone <68011813@kmitl.ac.th>
2026-02-16 09:20:06 +0700

    p01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,16 @@
 export class Student {}
+
+class Student {
+    firstname: string;
+    lastname: string;
+
+    constructor(firstname: string, lastname: string){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    printName(){
+        return `${this.firstname} ${this.lastname}`
+    }
+
+}
