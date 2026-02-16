fdddbbef348142789a9582f868838103a066dffb
srnTone <68011813@kmitl.ac.th>
2026-02-16 09:47:31 +0700

    u

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,10 @@
-export class Person {}
+export class Person {
+    firstname?: string;
+    lastname?: string;
+    private age?: number;
+
+    constructor(firstname?: string, lastname?: string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
